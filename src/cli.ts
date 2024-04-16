f015fc4f6f373fe5d3f3e1dc121f28745c98f660
Orchard <orchardyz@outlook.com>
2024-04-16 23:18:01 +0800

    feat: watch config file change

--- src/cli.ts
+++ src/cli.ts
@@ -1,9 +1,11 @@
 import { Command } from "commander";
-import { readFileSync } from "fs-extra";
+import { existsSync, readFileSync, watch } from "fs-extra";
 import { join } from "path";
 import chalk from "chalk";
 import generate from "./generate";
 import logger from "./logger";
+import { CONFIG_FILES } from "./constants";
+import { DefineConfigType } from "./types";
 
 const program = new Command();
 const version = JSON.parse(
@@ -15,10 +17,42 @@ const version = JSON.parse(
 logger.info(["Using", chalk.blueBright(`antd4-theme-vars@${version}`)]);
 
 export async function run() {
-  program.command("gen").action(function () {
-    logger.info("Start generating theme files...");
-    generate();
-  });
+  function genCommandAction(isRegenerate = false) {
+    logger.info(
+      isRegenerate
+        ? "Regenerating theme files..."
+        : "Start generating theme files..."
+    );
+    let configFilePath;
+    let config: DefineConfigType | undefined;
+    for (const configFile of CONFIG_FILES) {
+      configFilePath = join(process.cwd(), configFile); // 拼接当前工作目录和配置文件名
+      if (!existsSync(configFilePath)) continue; // 如果文件不存在，则继续遍历下一个配置文件
+      config = require(configFilePath); // 返回找到的配置文件内容
+    }
+
+    // 如果遍历完所有配置文件都未找到存在的配置文件，则结束进程
+    if (!config) {
+      logger.error(
+        "Cannot find the configuration file for `antd4-theme-vars`."
+      );
+      process.exit(1);
+    }
+
+    // 监听配置文件变化
+    watch(configFilePath!, (eventType, fileName) => {
+      if (eventType === "change") {
+        logger.info(
+          `${chalk.blueBright(fileName)} changed. Regenerating theme files...`
+        );
+        genCommandAction(true);
+      }
+    });
+
+    generate(config);
+  }
+
+  program.command("gen").action(() => genCommandAction());
 
   program.parse();
 }
--- src/generate.ts
+++ src/generate.ts
@@ -1,35 +1,9 @@
-import { existsSync } from "fs-extra";
-import {
-  CONFIG_FILES,
-  DEFAULT_ANT_PREFIX,
-  DEFAULT_OUTPUT_DIR,
-} from "./constants";
+import { DEFAULT_ANT_PREFIX, DEFAULT_OUTPUT_DIR } from "./constants";
 import { join } from "path";
 import { DefineConfigType, ThemeConfig } from "./types";
 import spawn from "cross-spawn";
 import logger from "./logger";
 import chalk from "chalk";
-
-/**
- * 获取配置文件的内容。
- * 该函数会遍历预定义的配置文件列表，查找当前工作目录下的第一个存在配置文件，并返回其内容。
- * 如果没有找到配置文件，则结束进程。
- *
- * @returns {DefineConfigType} 返回配置文件的内容。
- * @throws {Error} 如果找不到配置文件，则结束进程。
- */
-export function getConfig(): DefineConfigType {
-  // 遍历预定义的配置文件列表
-  for (const configFile of CONFIG_FILES) {
-    const filePath = join(process.cwd(), configFile); // 拼接当前工作目录和配置文件名
-    if (!existsSync(filePath)) continue; // 如果文件不存在，则继续遍历下一个配置文件
-    return require(filePath); // 返回找到的配置文件内容
-  }
-
-  // 如果遍历完所有配置文件都未找到存在的配置文件，则结束进程
-  logger.error("Cannot find the configuration file for `antd4-theme-vars`.");
-  process.exit(1);
-}
 
 export function uniqueThemeConfigs(themes: ThemeConfig[]) {
   // 使用Map来过滤掉重复的配置，以prefixCls为键
@@ -98,8 +72,7 @@ export async function outputThemeFile(config: ThemeConfig, targetDir: string) {
   }
 }
 
-export default async function generate() {
-  const config = getConfig();
+export default async function generate(config: DefineConfigType) {
   const { outputDir: _outputDir, themes: _themes } = config;
 
   const outputDir = _outputDir || DEFAULT_OUTPUT_DIR;
